1888f5622f0ecd267c90b1dd421494dbe19d784a
RossyWhite <daikishiroi@gmail.com>
2020-01-03 04:38:03 +0900

    Impl restore profile

--- awsctx.sh
+++ awsctx.sh
@@ -2,7 +2,7 @@
 
 AWSCTX="${XDG_CACHE_HOME:-$HOME/.aws}/awsctx"
 
-_usage() {
+_awsctx_usage() {
   cat <<"EOF"
 USAGE:
   awsctx                       : list the profiles
@@ -12,23 +12,33 @@ USAGE:
 EOF
 }
 
-_export_profile() {
+_awsctx_export_profile() {
   export AWS_PROFILE="${1}"
 }
 
-_persist_profile() {
+_awsctx_persist_profile() {
   echo "${1}" > "${AWSCTX}"
 }
 
-_set_profile() {
+_awsctx_restore_profile() {
+  local cached
+  if [[ -f "${AWSCTX}" ]]; then
+    cached="$(cat ${AWSCTX})"
+    if [[ -n "${cached}" ]]; then
+      _awsctx_export_profile "${cached}"
+    fi
+  fi
+}
+
+_awsctx_set_profile() {
   if [[ -n "$1" ]]; then
-    _export_profile "${1}"
-    _persist_profile "${1}"
+    _awsctx_export_profile "${1}"
+    _awsctx_persist_profile "${1}"
     echo Switched to profile \""${1}"\"
   fi
 }
 
-_list_profiles() {
+_awsctx_list_profiles() {
   local cmd
   cmd="$(_get_fzf_command)"
   eval "${cmd}"
@@ -41,11 +51,11 @@ _get_fzf_command() {
 }
 
 _choose_profile_interactive() {
-  local choice
+  local choice fzf_command
   fzf_command="$(_get_fzf_command)"
   choice=`FZF_DEFAULT_COMMAND="${fzf_command}" fzf --ansi`
    if [[ -n "${choice}" ]]; then
-    _set_profile "${choice}"
+    _awsctx_set_profile "${choice}"
    fi
 }
 
@@ -53,30 +63,25 @@ _choose_profile_interactive() {
 awsctx() {
   if [[ "$#" -gt 1 ]]; then
     echo "error: too many arguments" >&2
-    _usage
+    _awsctx_usage
   elif [[ "$#" -eq 1 ]]; then
     if [[ "${1}" == '-h' || "${1}" == '--help' ]]; then
-      _usage
+      _awsctx_usage
     elif [[ "${1}" =~ ^-(.*) ]]; then
       echo "error: unrecognized flag \"${1}\"" >&2
-      _usage
+      _awsctx_usage
     else
-      _set_profile "${1}"
+     _awsctx_set_profile "${1}"
     fi
   elif [[ "$#" -eq 0 ]]; then
     if [[ -t 1 &&  -z "${AWSCTX_IGNORE_FZF:-}" && "$(type fzf &>/dev/null; echo $?)" -eq 0 ]]; then
       _choose_profile_interactive
     else
-      _list_profiles
+      _awsctx_list_profiles
     fi
   else
-    _usage
+    _awsctx_usage
   fi
 }
 
-if [[ -f "${AWSCTX}" ]]; then
-  cached="$(cat ${AWSCTX})"
-  if [[ -n "${cached}" ]]; then
-    _export_profile "${cached}"
-  fi
-fi
+_awsctx_restore_profile
